a0a3d9d6b42b191a2fd4cc235fbb0ac3d7f3dbb1
looklsh <looklsh38@gmail.com>
2022-07-05 17:42:29 +0900

    useEffect 사용

--- src/index.js
+++ src/index.js
@@ -3,8 +3,10 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import TodoList from './TodoList1';
+import CoinTracker from './CoinTracker';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <TodoList />
+    // <TodoList />
+    <CoinTracker />
 );
