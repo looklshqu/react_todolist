b1d6c52624728ea707e824267835a9cf87cad54e
looklsh <looklsh38@gmail.com>
2022-07-05 18:10:13 +0900

    movie app 시작부분 에러

---
+++ src/Movie.js
@@ -0,0 +1,17 @@
+import { useEffect, useState } from "react";
+
+function Movie() {
+    const [loading, setLoading] = useState(true);
+    useEffect(() => {
+        fetch(`https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year`)
+        .then(response => response.json())
+        .then(json => console.log(json));
+    }, []);
+    return (
+        <div>
+            {loading ? <h1>loading...</h1> : null}
+        </div>
+    );
+}
+
+export default Movie;
--- src/index.js
+++ src/index.js
@@ -4,9 +4,11 @@ import './index.css';
 import App from './App';
 import TodoList from './TodoList1';
 import CoinTracker from './CoinTracker';
+import Movie from './Movie';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     // <TodoList />
-    <CoinTracker />
+    // <CoinTracker />
+    <Movie />
 );
